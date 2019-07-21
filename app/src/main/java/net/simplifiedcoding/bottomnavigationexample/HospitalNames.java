724b3bef2af1ddc6f2cc18487ce131c4c4b0eed3
awang085 <50627636+awang085@users.noreply.github.com>
2019-07-21 15:14:28 -0400

    Update HospitalNames.java

--- app/src/main/java/net/simplifiedcoding/bottomnavigationexample/HospitalNames.java
+++ app/src/main/java/net/simplifiedcoding/bottomnavigationexample/HospitalNames.java
@@ -1,13 +1,20 @@
 package net.simplifiedcoding.bottomnavigationexample;
 
+/**
+ * Created by Anbo.
+ * Class of hospital name
+ */
+
 public class HospitalNames {
     private String name;
 
     public HospitalNames(String name) {
+        //Constructor
         this.name = name;
     }
 
     public String getHospitalName() {
+        //getter
         return this.name;
     }
 
